e28f1cbc79e7ced8f4277a18fae2f525a7a868fd
zornitsa25 <zchopova@gmail.com>
2015-08-30 20:18:26 +0300

    adding SquareRoot.cs

---
+++ Intro-Basics/SquareRoot.cs
@@ -0,0 +1,10 @@
+﻿using System;
+class SquareRoot
+{
+    static void Main(string[] args)
+    {
+        short n = 12345;
+        double sqrt = Math.Sqrt(n);
+        Console.WriteLine(sqrt);
+    }
+}
