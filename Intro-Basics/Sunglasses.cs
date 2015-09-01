a1ccd6911a4dda15488641b2b92807489534f23c
zornitsa25 <zchopova@gmail.com>
2015-09-01 12:49:11 +0300

    Intro Programming with c#

--- Intro-Basics/Sunglasses.cs
+++ Intro-Basics/Sunglasses.cs
@@ -2,67 +2,40 @@
 
 class Sunglasses
 {
-    static void Asterisks()
-    {
-        Console.Write('*');
-    }
-    static void Space()
-    {
-        Console.Write(' ');
-    }
-
     static void FirstLastRow(int n)
     {
         int Ast = 2 * n;
-        for (int i = 0; i < Ast; i++)
-        {
-            Asterisks();
-        }
-        for(int i=0;i<n;i++)
-        {
-            Space();
-        }
-        for (int i = 0; i < Ast; i++)
-        {
-            Asterisks();
-        }
+        Console.Write(new string('*', Ast));
+        Console.Write(new string(' ', n));
+        Console.Write(new string('*',Ast));
+
         Console.WriteLine();
     }
     static void Middle(int n)
     {
         char ast = '*';
-
         for (int i = 0; i < n-2;i++)
         {
             Console.Write(ast);
-            for (int j = 0; j < 2 * n - 2; j++)
-            {
-                Console.Write('/');
-            }
+            Console.Write(new string('/', 2 * n - 2));
             Console.Write(ast);
             if(i==(n-3)/2)
             {
-                for (int k = 0; k < n;k++)
-                    Console.Write('|');
+                Console.Write(new string('|', n));
             }
             else
             {
-                for (int p = 0; p < n; p++)
-                {
-                    Space();
-                }
+                Console.Write(new string(' ', n));
             }
             Console.Write(ast);
-            for (int j = 0; j < 2 * n - 2; j++)
-            {
-                Console.Write('/');
-            }
+            Console.Write(new string('/', 2 * n - 2));
             Console.Write(ast);
             Console.WriteLine();
         }
     }
     static void Main()
     {
+        // n is an odd number
         int n = int.Parse(Console.ReadLine());
         FirstLastRow(n);
         Middle(n);
